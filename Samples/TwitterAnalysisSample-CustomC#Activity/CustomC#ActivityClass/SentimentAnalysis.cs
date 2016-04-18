ef5d0b78a10ac70070f0e9b348395434ec7dc182
Aaron Kim <ahkim@ahkim.com>
2016-04-18 21:44:38 +0800

    Update SentimentAnalysis.cs
    Updated the SentimentAnalysis class to inherit latest IDotNetActivity from obsolete ICustomActivity.

--- Samples/TwitterAnalysisSample-CustomC#Activity/CustomC#ActivityClass/SentimentAnalysis.cs
+++ Samples/TwitterAnalysisSample-CustomC#Activity/CustomC#ActivityClass/SentimentAnalysis.cs
@@ -5,15 +5,18 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
+using System.Net.Http.Formatting;
+
+using System.Linq;
 using Microsoft.Azure.Management.DataFactories.Models;
-using Microsoft.DataFactories.Runtime;
+using Microsoft.Azure.Management.DataFactories.Runtime;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace SentimentAnalysisService
 {
-    public class SentimentAnalysis : ICustomActivity
+    public class SentimentAnalysis : IDotNetActivity
     {
         private IActivityLogger _logger;
         private string _storageConnectionString;
@@ -24,70 +27,97 @@ namespace SentimentAnalysisService
         private string _baseUrl;
 
         public IDictionary<string, string> Execute(
-           IEnumerable<ResolvedTable> inputTables,
-           IEnumerable<ResolvedTable> outputTables,
-           IDictionary<string, string> inputs,
-           IActivityLogger activityLogger)
+            IEnumerable<LinkedService> linkedServices, 
+            IEnumerable<Dataset> datasets, 
+            Activity activity, 
+            IActivityLogger logger)
         {
-            _logger = activityLogger;
-            string sliceStartTime = inputs["sliceStart"];
-            _logger.Write(TraceEventType.Information, "Slice start time is : {0}", sliceStartTime);
+            _logger = logger;
 
-            _baseUrl = inputs["baseUrl"];
+            _logger.Write("######Execute Begin######");
+
+            // to get extended properties (for example: SliceStart)
+            DotNetActivity dotNetActivity = (DotNetActivity)activity.TypeProperties;
+            string sliceStartTime = dotNetActivity.ExtendedProperties["SliceStart"];
+            _logger.Write("Slice start time is : {0}", sliceStartTime);
+
+            _baseUrl = dotNetActivity.ExtendedProperties["baseUrl"];
             if (String.IsNullOrEmpty(_baseUrl))
             {
-                _logger.Write(TraceEventType.Error, "Null or Empty Base URL for ML Model: {0}", _baseUrl);
+                _logger.Write("Null or Empty Base URL for ML Model: {0}", _baseUrl);
                 throw new Exception(string.Format("Null or Empty Base URL for ML Model: {0}", _baseUrl));
             }
-            _logger.Write(TraceEventType.Information, "Base ML Azure Website url is : {0}", _baseUrl);
+            _logger.Write("Base ML Azure Website url is : {0}", _baseUrl);
 
-            _apiKey = inputs["apiKey"];
+            _apiKey = dotNetActivity.ExtendedProperties["apiKey"];
             if (String.IsNullOrEmpty(_apiKey))
             {
-                _logger.Write(TraceEventType.Error, "Null or Empty API Key for ML Model: {0}", _apiKey);
+                _logger.Write("Null or Empty API Key for ML Model: {0}", _apiKey);
                 throw new Exception(string.Format("Null or Empty API Key for ML Model: {0}", _apiKey));
             }
 
-            // Get the ADF Input Tables
-            foreach (var inputTable in inputTables)
-            {
-                _storageConnectionString = GetConnectionString(inputTable.LinkedService);
-                string folderPath = GetFolderPath(inputTable.Table);
+            // declare dataset types
+            CustomDataset inputLocation;
+            CustomDataset outputLocation;
+            AzureStorageLinkedService inputLinkedService;
+            AzureStorageLinkedService outputLinkedService;
 
-                if (String.IsNullOrEmpty(_storageConnectionString))
-                {
-                    _logger.Write(TraceEventType.Error, "Null or Empty Connection string for input table: {0}", inputTable);
-                    throw new Exception(string.Format("Null or Empty Connection string for input table: {0}", inputTable));
-                }
-                if (String.IsNullOrEmpty(folderPath))
-                {
-                    _logger.Write(TraceEventType.Error, "Null or Empty folderpath for input table: {0}", inputTable);
-                    throw new Exception(string.Format("Null or Empty folder path for input table: {0}", inputTable));
-                }
-                _storageContainerName = folderPath.Split('/')[0];
-                _inputBlobName = folderPath.Substring(folderPath.IndexOf('/') + 1) ;
-                _logger.Write(TraceEventType.Information, "Folder Path for Input Table {0}: {1}", inputTable, folderPath);
+            // Get the ADF Input Tables
+            Dataset inputDataset = datasets.Single(dataset => dataset.Name == activity.Inputs.Single().Name);
+            inputLocation = inputDataset.Properties.TypeProperties as CustomDataset;
+
+            inputLinkedService = linkedServices.Single(
+                linkedService =>
+                linkedService.Name ==
+                inputDataset.Properties.LinkedServiceName).Properties.TypeProperties
+                as AzureStorageLinkedService;
+
+            _storageConnectionString = inputLinkedService.ConnectionString;
+
+            if (String.IsNullOrEmpty(_storageConnectionString))
+            {
+                _logger.Write("Null or Empty Connection string for input table: {0}", inputDataset.Name);
+                throw new Exception(string.Format("Null or Empty Connection string for input table: {0}", inputDataset.Name));
             }
+
+            string folderPath = GetFolderPath(inputDataset);
+
+            if (String.IsNullOrEmpty(folderPath))
+            {
+                _logger.Write("Null or Empty folderpath for input table: {0}", inputDataset.Name);
+                throw new Exception(string.Format("Null or Empty folder path for input table: {0}", inputDataset.Name));
+            }
+            _storageContainerName = folderPath.Split('/')[0];
+            _inputBlobName = folderPath.Substring(folderPath.IndexOf('/') + 1) ;
+            _logger.Write("Folder Path for Input Table {0}: {1}", inputDataset.Name, folderPath);            
 
             // Get the ADF Output Tables
-            foreach (var outputTable in outputTables)
-            {
-                _storageConnectionString = GetConnectionString(outputTable.LinkedService);
-                string folderPath = GetFolderPath(outputTable.Table);
+            Dataset outputDataset = datasets.Single(dataset => dataset.Name == activity.Outputs.Single().Name);
+            outputLocation = outputDataset.Properties.TypeProperties as CustomDataset;
 
-                if (String.IsNullOrEmpty(_storageConnectionString))
-                {
-                    _logger.Write(TraceEventType.Error, "Null or Empty Connection string for output table: {0}", outputTable);
-                    throw new Exception(string.Format("Null or Empty Connection string for output table: {0}", outputTable));
-                }
-                if (String.IsNullOrEmpty(folderPath))
-                {
-                    _logger.Write(TraceEventType.Error, "Null or Empty folderpath for output table: {0}", outputTable);
-                    throw new Exception(string.Format("Null or Empty folder path for output table: {0}", outputTable));
-                }
-                _outputBlobName = folderPath.Substring(folderPath.IndexOf('/') + 1) ;
-                _logger.Write(TraceEventType.Information, "Folder Path for Ouput Table {0}: {1}", outputTable, folderPath);
+            outputLinkedService = linkedServices.Single(
+                linkedService =>
+                linkedService.Name ==
+                outputDataset.Properties.LinkedServiceName).Properties.TypeProperties
+                as AzureStorageLinkedService;
+
+            _storageConnectionString = outputLinkedService.ConnectionString;
+
+            folderPath = GetFolderPath(outputDataset);
+
+            if (String.IsNullOrEmpty(_storageConnectionString))
+            {
+                _logger.Write("Null or Empty Connection string for output table: {0}", outputDataset.Name);
+                throw new Exception(string.Format("Null or Empty Connection string for output table: {0}", outputDataset.Name));
             }
+            if (String.IsNullOrEmpty(folderPath))
+            {
+                _logger.Write("Null or Empty folderpath for output table: {0}", outputDataset.Name);
+                throw new Exception(string.Format("Null or Empty folder path for output table: {0}", outputDataset.Name));
+            }
+            _outputBlobName = folderPath.Substring(folderPath.IndexOf('/') + 1) ;
+            _logger.Write("Folder Path for Ouput Table {0}: {1}", outputDataset.Name, folderPath);
+            
          
             try
             {
@@ -96,7 +126,7 @@ namespace SentimentAnalysisService
             }
             catch (Exception ex)
             {
-                _logger.Write(TraceEventType.Error, "ML Model Call failed with error : {0}", ex.ToString());
+                _logger.Write("ML Model Call failed with error : {0}", ex.ToString());
                 throw;
             }
 
@@ -116,7 +146,7 @@ namespace SentimentAnalysisService
             var container = blobClient.GetContainerReference(_storageContainerName);
             var blob = container.GetBlockBlobReference(_inputBlobName);
 
-            _logger.Write(TraceEventType.Information, "Submitting the job...");
+            _logger.Write("Submitting the job...");
             
             // set a time out for polling status
             const int timeOutInMilliseconds = 120 * 100000; // Set a timeout
@@ -147,32 +177,32 @@ namespace SentimentAnalysisService
                     if (watch.ElapsedMilliseconds > timeOutInMilliseconds)
                     {
                         done = true;
-                        _logger.Write(TraceEventType.Information, "Timed out. Deleting the job ...");
+                        _logger.Write("Timed out. Deleting the job ...");
                         await client.DeleteAsync(jobLocation);
                     }
                     switch (status.StatusCode)
                     {
                         case BatchScoreStatusCode.NotStarted:
-                            _logger.Write(TraceEventType.Information, "Not started...");
+                            _logger.Write("Not started...");
                             break;
                         case BatchScoreStatusCode.Running:
-                            _logger.Write(TraceEventType.Information, "Running...");
+                            _logger.Write("Running...");
                             break;
                         case BatchScoreStatusCode.Failed:
-                            _logger.Write(TraceEventType.Error, "Failed!");
-                            _logger.Write(TraceEventType.Error, "Error details : {0}", status.Details);
+                            _logger.Write("Failed!");
+                            _logger.Write("Error details : {0}", status.Details);
                             throw new Exception(status.Details);
                         case BatchScoreStatusCode.Cancelled:
-                            _logger.Write(TraceEventType.Information, "Cancelled!");
+                            _logger.Write("Cancelled!");
                             throw new Exception(status.Details);
                         case BatchScoreStatusCode.Finished:
                             done = true;
-                            _logger.Write(TraceEventType.Information, "Finished!");
+                            _logger.Write("Finished!");
                             var credentials = new StorageCredentials(status.Result.SasBlobToken);
                             var sourceCloudBlob = new CloudBlockBlob(new Uri(new Uri(status.Result.BaseLocation), status.Result.RelativeLocation), credentials);
                             var targetCloudBlob = container.GetBlockBlobReference(_outputBlobName);
-                            targetCloudBlob.StartCopyFromBlob(sourceCloudBlob);
-                            _logger.Write(TraceEventType.Information, "Copy to Output Blob Complete...");
+                            targetCloudBlob.StartCopy(sourceCloudBlob);
+                            _logger.Write("Copy to Output Blob Complete...");
                             break;
                     }
 
@@ -184,38 +214,24 @@ namespace SentimentAnalysisService
             }
         }
 
-        // Get Linked Service Connection String
-        private static string GetConnectionString(LinkedService asset)
+
+        /// <summary>
+        /// Gets the folderPath value from the input/output dataset.
+        /// </summary>
+        private static string GetFolderPath(Dataset dataArtifact)
         {
-            if (asset == null)
+            if (dataArtifact == null || dataArtifact.Properties == null)
             {
                 return null;
             }
 
-            AzureStorageLinkedService storageAsset = asset.Properties as AzureStorageLinkedService;
-            if (storageAsset == null)
+            AzureBlobDataset blobDataset = dataArtifact.Properties.TypeProperties as AzureBlobDataset;
+            if (blobDataset == null)
             {
                 return null;
             }
 
-            return storageAsset.ConnectionString;
-        }
-
-        // Get Folder Path for table
-        private static string GetFolderPath(Table table)
-        {
-            if (table == null || table.Properties == null)
-            {
-                return null;
-            }
-
-            AzureBlobLocation blobLocation = table.Properties.Location as AzureBlobLocation;
-            if (blobLocation == null)
-            {
-                return null;
-            }
-
-            return blobLocation.FolderPath + blobLocation.FileName;
+            return blobDataset.FolderPath;
         }
     }
 
