3dc72e9b60742c2e1452c95f2d3133e512575475
gauravmalhot <gaur123_malhot@hotmail.com>
2016-12-01 00:23:34 -0800

    Code Refactor for DeleteBlobFileFolder custom activity sample
    Code Refactor for DeleteBlobFileFolder custom activity sample

--- Samples/DeleteBlobFileFolderCustomActivity/DeleteFromBlobActivity.cs
+++ Samples/DeleteBlobFileFolderCustomActivity/DeleteFromBlobActivity.cs
@@ -10,7 +10,7 @@ using Microsoft.Azure.Management.DataFactories.Runtime;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 
-namespace ODSPADFCustomActivity
+namespace ADF.Sample.DeleteFromBlobCustomActivity
 {
     public class DeleteFromBlobActivity : IDotNetActivity
     {
@@ -45,57 +45,7 @@ namespace ODSPADFCustomActivity
 
                 List<string> dataSetsToDelete = inputToDelete.Split(',').ToList();
 
-                foreach (string strInputToDelete in dataSetsToDelete)
-                {
-                    Dataset inputDataset = datasets.First(ds => ds.Name.Equals(strInputToDelete));
-                    AzureBlobDataset blobDataset = inputDataset.Properties.TypeProperties as AzureBlobDataset;
-                    logger.Write("\nBlob folder: " + blobDataset.FolderPath);
-                    logger.Write("\nBlob file: " + blobDataset.FileName);
-                    
-                    // linked service for input and output is the same.
-                    AzureStorageLinkedService linkedService = linkedServices.First(ls =>
-                        ls.Name == inputDataset.Properties.LinkedServiceName).Properties.TypeProperties as AzureStorageLinkedService;
-
-                    // create storage client for input. Pass the connection string.
-                    CloudStorageAccount storageAccount = CloudStorageAccount.Parse(linkedService.ConnectionString);
-                    CloudBlobClient client = storageAccount.CreateCloudBlobClient();
-
-                    // find blob to delete and delete if exists.
-                    Uri blobUri = new Uri(storageAccount.BlobEndpoint, blobDataset.FolderPath + blobDataset.FileName);
-                    CloudBlockBlob blob = new CloudBlockBlob(blobUri, storageAccount.Credentials);
-                    logger.Write("Blob Uri: {0}",blobUri.AbsoluteUri);
-                    logger.Write("Blob exists: {0}", blob.Exists());
-                    blob.DeleteIfExists();
-                    logger.Write("Deleted blob: {0}",blobUri.AbsoluteUri);
-
-                    // Ensure the container is exist.
-                    if (blobDataset.FolderPath.IndexOf("/") > 0)
-                    {
-                        string containerName = blobDataset.FolderPath.Substring(0, blobDataset.FolderPath.IndexOf("/"));
-                        logger.Write("Container Name {0}", containerName);
-
-                        string directoryName = blobDataset.FolderPath.Substring(blobDataset.FolderPath.IndexOf("/") + 1);
-                        logger.Write("Directory Name {0}", directoryName);
-
-                        var blobContainer = client.GetContainerReference(containerName);
-                        blobContainer.CreateIfNotExists();
-                        CloudBlobDirectory cbd = blobContainer.GetDirectoryReference(directoryName);
-
-                        foreach (IListBlobItem item in blobContainer.ListBlobs(directoryName, true))
-                        {
-                            logger.Write("Blob Uri: {0} ", item.Uri.AbsoluteUri);
-                            
-                            if (item.GetType() == typeof(CloudBlockBlob) || item.GetType().BaseType == typeof(CloudBlockBlob))
-                            {
-                                CloudBlockBlob subBlob = new CloudBlockBlob(item.Uri, storageAccount.Credentials);
-                                logger.Write("Blob exists: {0}",subBlob.Exists());
-                                subBlob.DeleteIfExists();
-                                logger.Write("Deleted blob {0}",item.Uri.AbsoluteUri);
-                            }
-                        }
-                    }
-                    
-                }
+                DeleteBlobFileFolder(dataSetsToDelete);
 
                 logger.Write("Custom Activity Ended Successfully.");
             }
@@ -110,6 +60,65 @@ namespace ODSPADFCustomActivity
             // The dictionary can be used to chain custom activities together in the future.
             // This feature is not implemented yet, so just return an empty dictionary.
             return new Dictionary<string, string>();
+        }
+
+        /// <summary>
+        /// Delete azure blob file or entire folder
+        /// </summary>
+        /// <param name="dataSetsToDelete"></param>
+        /// 
+        public void DeleteBlobFileFolder(List<string> dataSetsToDelete)
+        {
+            foreach (string strInputToDelete in dataSetsToDelete)
+            {
+                Dataset inputDataset = datasets.First(ds => ds.Name.Equals(strInputToDelete));
+                AzureBlobDataset blobDataset = inputDataset.Properties.TypeProperties as AzureBlobDataset;
+                logger.Write("\nBlob folder: " + blobDataset.FolderPath);
+                logger.Write("\nBlob file: " + blobDataset.FileName);
+
+                // linked service for input and output is the same.
+                AzureStorageLinkedService linkedService = linkedServices.First(ls =>
+                    ls.Name == inputDataset.Properties.LinkedServiceName).Properties.TypeProperties as AzureStorageLinkedService;
+
+                // create storage client for input. Pass the connection string.
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(linkedService.ConnectionString);
+                CloudBlobClient client = storageAccount.CreateCloudBlobClient();
+
+                // find blob to delete and delete if exists.
+                Uri blobUri = new Uri(storageAccount.BlobEndpoint, blobDataset.FolderPath + blobDataset.FileName);
+                CloudBlockBlob blob = new CloudBlockBlob(blobUri, storageAccount.Credentials);
+                logger.Write("Blob Uri: {0}", blobUri.AbsoluteUri);
+                logger.Write("Blob exists: {0}", blob.Exists());
+                blob.DeleteIfExists();
+                logger.Write("Deleted blob: {0}", blobUri.AbsoluteUri);
+
+                // Ensure the container is exist.
+                if (blobDataset.FolderPath.IndexOf("/") > 0)
+                {
+                    string containerName = blobDataset.FolderPath.Substring(0, blobDataset.FolderPath.IndexOf("/"));
+                    logger.Write("Container Name {0}", containerName);
+
+                    string directoryName = blobDataset.FolderPath.Substring(blobDataset.FolderPath.IndexOf("/") + 1);
+                    logger.Write("Directory Name {0}", directoryName);
+
+                    var blobContainer = client.GetContainerReference(containerName);
+                    blobContainer.CreateIfNotExists();
+                    CloudBlobDirectory cbd = blobContainer.GetDirectoryReference(directoryName);
+
+                    foreach (IListBlobItem item in blobContainer.ListBlobs(directoryName, true))
+                    {
+                        logger.Write("Blob Uri: {0} ", item.Uri.AbsoluteUri);
+
+                        if (item.GetType() == typeof(CloudBlockBlob) || item.GetType().BaseType == typeof(CloudBlockBlob))
+                        {
+                            CloudBlockBlob subBlob = new CloudBlockBlob(item.Uri, storageAccount.Credentials);
+                            logger.Write("Blob exists: {0}", subBlob.Exists());
+                            subBlob.DeleteIfExists();
+                            logger.Write("Deleted blob {0}", item.Uri.AbsoluteUri);
+                        }
+                    }
+                }
+            }
         }
     }
 }
