08ad9e4282e9da13c3284103307613900317b42c
charith ekanayake <charithforex@gmail.com>
2021-05-30 23:15:03 +0530

    SFTP MetaData Reader

---
+++ SamplesV2/SFTPLatestMetaDataReader/tablescript.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[FileReader_MetaData](
+	[FileType] [varchar](100) NULL,
+	[Source] [varchar](100) NULL,
+	[FileName] [varchar](100) NULL,
+	[DateTimeUTC] [datetime] NULL
+) ON [PRIMARY]
+GO
+
