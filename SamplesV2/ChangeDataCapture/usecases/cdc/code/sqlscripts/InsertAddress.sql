3adf3785a38a98649e2633b9b26eb58f10e2be0f
datasnowman <darsch@microsoft.com>
2021-08-06 18:43:34 -0700

    adbsteps

---
+++ SamplesV2/ChangeDataCapture/usecases/cdc/code/sqlscripts/InsertAddress.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO [SalesLT].[Address]
+        ([AddressLine1], [AddressLine2], [City], [StateProvince] ,[CountryRegion], [PostalCode])
+ 		VALUES
+		('138th Drive', NULL, 'Everett', 'WA', 'USA', '98208')
