f89aaf48340d08faeb67839fa08214706337ae41
jodobrze <jodobrze@microsoft.com>
2023-03-14 13:38:40 -0500

    Removed old commented code

--- SamplesV2/PastRunDetails/PastRunDetails_ActivityLevelCSV.ps1
+++ SamplesV2/PastRunDetails/PastRunDetails_ActivityLevelCSV.ps1
@@ -1,7 +1,7 @@
 $dataFactoryName = ""
 $resourceGroupName = ""
 $startTime = "1/12/2023 00:00:00"
-$endTime = "1/13/2023 12:00:00"
+$endTime = "1/13/2023 00:00:00"
 $filePath = "C:\temp\adfouput.csv"
 
 $pipelineRuns = Get-AzDataFactoryV2PipelineRun -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -LastUpdatedAfter $startTime -LastUpdatedBefore $endTime
@@ -13,8 +13,8 @@ foreach($pipelineRun in $pipelineRuns) {
     $activtiyRuns = Get-AzDataFactoryV2ActivityRun -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -pipelineRunId $pipelineRun.RunId -RunStartedAfter $startTime -RunStartedBefore $endTime
     
     foreach($activtiyRun in $activtiyRuns) {
-        if ($activtiyRun.Output -ne $null -and
-                $activtiyRun.Output.SelectToken("billingReference.billableDuration") -ne $null) {
+        if ($null -ne $activtiyRun.Output -and
+                $null -ne $activtiyRun.Output.SelectToken("billingReference.billableDuration")) {
             
             $billingReference = $activtiyRun.Output.SelectToken("billingReference.billableDuration").ToString() | ConvertFrom-Json
             $activityBillingType = $activtiyRun.Output.SelectToken("billingReference.activityType").ToString()
@@ -31,19 +31,6 @@ foreach($pipelineRun in $pipelineRuns) {
                 pipelineName = $pipelineRun.PipelineName
                 dataFactoryName = $pipelineRun.DataFactoryName
             }
-            
-            # $x = @()
-            # $x = $activtiyRun.Output.SelectToken("billingReference.billableDuration").ToString() | ConvertFrom-Json
-            # $x | Add-Member -MemberType NoteProperty -Name "activityBillingType" -Value $activtiyRun.Output.SelectToken("billingReference.activityType").ToString()
-            # $x | Add-Member -MemberType NoteProperty -Name "activityType" -Value $activtiyRun.ActivityType.ToString()
-            # $x | Add-Member -MemberType NoteProperty -Name "activityName" -Value $activtiyRun.ActivityName.ToString()
-            # $x | Add-Member -MemberType NoteProperty -Name "activityRunStart" -Value $activtiyRun.ActivityRunStart.ToString()
-            # $x | Add-Member -MemberType NoteProperty -Name "pipelineRunId" -Value $pipelineRun.RunId
-            # $x | Add-Member -MemberType NoteProperty -Name "pipelineName" -Value $pipelineRun.PipelineName
-            # $x | Add-Member -MemberType NoteProperty -Name "dataFactoryName" -Value $pipelineRun.DataFactoryName
-
-            # $activityDetails += $x
-
         }
     }
 }
